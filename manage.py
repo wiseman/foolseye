685d3902698c24e216739d483c3cfd4d81e525dd
wiseman <jjwiseman@gmail.com>
2010-10-31 12:56:38 -0700

    Changed runserver command to start server in threaded mode.

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 import subprocess
 
+import flaskext.script
 from flaskext.script import Manager
 
 from shopcop import app
 
 manager = Manager(app)
+
+
+manager.add_command('runserver', flaskext.script.Server(threaded=True))
 
 
 UPLOAD_URL = 'http://localhost:5000/upload'
@@ -14,8 +18,9 @@ def upload_image(path):
   args = ['curl',
           '-F', 'file=@%s' % (path,), UPLOAD_URL]
   status = subprocess.call(args)
-  if status != 0:
-    raise ValueError('Attempt to run %s failed with status=%s' % (args, status))
+  return status
+
+
 
 
 
