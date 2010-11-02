e30860cbdfdf5cd8e3419f682d7b213cb65f3f10
wiseman <jjwiseman@gmail.com>
2010-11-01 23:43:51 -0700

    Renamed upload_image to uploadimage (like runserver).

--- manage.py
+++ manage.py
@@ -14,7 +14,7 @@ manager.add_command('runserver', flaskext.script.Server(threaded=True))
 UPLOAD_URL = 'http://localhost:5000/upload'
 
 @manager.command
-def upload_image(path):
+def uploadimage(path):
     args = ['curl',
             '-F', 'file=@%s' % (path,), UPLOAD_URL]
     status = subprocess.call(args)
